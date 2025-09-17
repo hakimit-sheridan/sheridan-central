4065364bf16396afc5a53dbb989ca74f6d8db880
Tamim Hakimi <hakimit@sheridancollege.ca>
2025-09-17 15:55:14 -0400

    removed unneeded files

--- 10004-prog/stadium.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-
-def setupStadium(raceLength: int):
-    stadium = turtle.Screen()
-    stadium.bgcolor("lightgreen")
-    stadium.screensize(raceLength * 2, raceLength * 2)
-    return stadium
